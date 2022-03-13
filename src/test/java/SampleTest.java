eebb6821b2cc2b142c2dd060f03f052d76b4e831
ravi1149 <raviswork@outlook.com>
2022-03-13 10:53:32 +0530

    initial commit

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 public class SampleTest {
     @Test
     public void get(){
-        WebDriverManager.edgedriver().setup();
-        WebDriver web = new EdgeDriver();
-        web.get("https://www.google.com/");
+//        WebDriverManager.edgedriver().setup();
+//        WebDriver web = new EdgeDriver();
+//        web.get("https://www.google.com/");
         System.out.println("Hello world");
     }
 }
