7130a5005fe5d9ee852ac31d137d6cea6c0a7e35
ravi1149 <raviswork@outlook.com>
2022-03-22 10:50:29 +0530

    initial commit

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,14 +1,20 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
 
 public class SampleTest {
     @Test
     public void get(){
-//        WebDriverManager.edgedriver().setup();
-//        WebDriver web = new EdgeDriver();
-//        web.get("https://www.google.com/");
-        System.out.println("Hello world");
+        WebDriverManager.edgedriver().setup();
+        WebDriver  driver= new EdgeDriver();
+        driver.get("https://outlook.live.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/header/div/aside/div/nav/ul/li[2]/a"));
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.quit();
     }
 }
